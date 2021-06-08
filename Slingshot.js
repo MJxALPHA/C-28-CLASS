dc704b7e18756c2dfa2257a9fe3fcdf61e0c518a
MJxALPHA <78998499+MJxALPHA@users.noreply.github.com>
2021-06-08 12:33:32 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,24 @@
+class Slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.slingShot = Constraint.create(options);
+        this.pointB = pointB;
+        World.add(world, this.slingShot);
+    }
+    fly(){
+      this.slingShot.bodyA = null;
+    }
+    display(){
+       if(this.slingShot.bodyA){
+        var pointA = this.slingShot.bodyA.position;
+        var pointB = this.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+       }       
+    }
+}
